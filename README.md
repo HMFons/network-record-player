11a549bcbe906a747c4688f8acbdd924b2a44e04
HMFons <6265638+HMFons@users.noreply.github.com>
2024-05-05 22:50:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # network-record-player
 Scan nfc stickers to play music from network storage
+
+## dependencies
+pip install python-vlc
+
+### dev dependencies
+pip3 install Mock.GPIO
---
+++ main.py
@@ -0,0 +1,36 @@
+# from Mock.GPIO import GPIO
+import vlc
+import time
+import os
+
+PATH=""
+listPlayer = vlc.MediaListPlayer()
+
+def useCommand(command, listPlayer): 
+    print(command)
+    if(command=="pause"): listPlayer.pause()
+    elif(command=="play"): listPlayer.play()
+    elif(command=="next"): listPlayer.next()
+    elif(command=="back"): listPlayer.previous()
+    elif(command=="replay"): 
+        listPlayer.previous()
+        listPlayer.next()
+    elif(command.endswith('.mp3') or command.endswith('.m4a')or command.endswith('.flac')):
+        mediaList = vlc.MediaList()
+        mediaList.add_media(vlc.Media(os.path.join(PATH,command)))
+        listPlayer.set_media_list(mediaList)
+        print(listPlayer)
+        listPlayer.play()
+    else: 
+        mediaList = vlc.MediaList()
+        path = PATH+command
+        songs = os.listdir(path)
+        for s in songs:
+            mediaList.add_media(vlc.Media(os.path.join(path,s)))
+        listPlayer.set_media_list(mediaList)
+        print(listPlayer)
+        listPlayer.play()    
+while True:
+    print("Wat do?")
+    useCommand(input(),listPlayer)
+    time.sleep(2)
